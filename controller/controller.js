8609569175062c10728d6bf5cda69299fbe3cac3
Yoko <baghram0001@gmail.com>
2020-04-02 22:15:14 +0700

    Added Controller GetData

--- controller/controller.js
+++ controller/controller.js
@@ -3,7 +3,20 @@ const Axios = require('axios')
 class Controller {
 
     static GetData(req, res) {
-        
+        let Province = req.body.Province
+        let data;
+        Axios({
+            url: 'https://api.kawalcorona.com/indonesia/provinsi/',
+            method:"GET"
+        })
+            .then(function(result) {   
+                for(let i = 0; i < result; i++) {
+                    if(result[i].Provinsi == Province) {
+                        data = result[i]
+                    }
+                }
+                res.status(200).json(data)
+            })
     }
 }
 
